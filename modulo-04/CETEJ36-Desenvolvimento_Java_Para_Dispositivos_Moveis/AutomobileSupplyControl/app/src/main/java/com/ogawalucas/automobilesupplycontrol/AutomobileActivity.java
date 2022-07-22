75ff138ed4362f2f4d8551c838139f32d4b6351e
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-07-22 08:50:44 -0300

    Adding more EditTexts

--- modulo-04/CETEJ36-Desenvolvimento_Java_Para_Dispositivos_Moveis/AutomobileSupplyControl/app/src/main/java/com/ogawalucas/automobilesupplycontrol/AutomobileActivity.java
+++ modulo-04/CETEJ36-Desenvolvimento_Java_Para_Dispositivos_Moveis/AutomobileSupplyControl/app/src/main/java/com/ogawalucas/automobilesupplycontrol/AutomobileActivity.java
@@ -12,17 +12,21 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Objects;
-
 public class AutomobileActivity extends AppCompatActivity {
 
     private TextView tvNickname;
-    private EditText edNickname;
+    private EditText etNickname;
     private CheckBox cbTravelCar;
     private TextView tvAutomobileType;
     private RadioGroup rgAutomobileType;
     private TextView tvBrand;
     private Spinner spBrand;
+    private TextView tvModel;
+    private EditText etModel;
+    private TextView tvColor;
+    private EditText etColor;
+    private TextView tvManufactoringYear;
+    private EditText etManufactoringYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +39,24 @@ public class AutomobileActivity extends AppCompatActivity {
 
     private void mapAttributes() {
         tvNickname = findViewById(R.id.tvNickname);
-        edNickname = findViewById(R.id.etNickname);
+        etNickname = findViewById(R.id.etNickname);
+
         cbTravelCar = findViewById(R.id.cbTravelCar);
+
         tvAutomobileType = findViewById(R.id.tvAutomobileCar);
         rgAutomobileType = findViewById(R.id.rgAutomobileCar);
+
         tvBrand = findViewById(R.id.tvBrand);
         spBrand = findViewById(R.id.spBrand);
+
+        tvModel = findViewById(R.id.tvModel);
+        etModel = findViewById(R.id.etModel);
+
+        tvColor = findViewById(R.id.tvColor);
+        etColor = findViewById(R.id.etColor);
+
+        tvManufactoringYear = findViewById(R.id.tvManufactoringYear);
+        etManufactoringYear = findViewById(R.id.etManufactoringYear);
     }
 
     private void setSpinners() {
@@ -53,21 +69,48 @@ public class AutomobileActivity extends AppCompatActivity {
     }
 
     public void clearEditTexts(View view) {
-        edNickname.setText(null);
+        etNickname.setText(null);
         cbTravelCar.setChecked(false);
         rgAutomobileType.clearCheck();
+        etModel.setText(null);
+        etColor.setText(null);
+        etManufactoringYear.setText(null);
 
         showToast(getString(R.string.cleaned), Toast.LENGTH_SHORT);
     }
 
     public void validateFields(View view) {
         validateNickname();
+        validateModel();
+        validateColor();
+        validateManufactoringYear();
     }
 
     private void validateNickname() {
-        if (edNickname.getText() == null || edNickname.getText().length() == 0) {
+        if (etNickname.getText() == null || etNickname.getText().length() == 0) {
             showToast(String.format("%s: %s.", getString(R.string.empty_fields), tvNickname.getText()), Toast.LENGTH_LONG);
-            edNickname.requestFocus();
+            etNickname.requestFocus();
+        }
+    }
+
+    private void validateModel() {
+        if (etModel.getText() == null || etNickname.getText().length() == 0) {
+            showToast(String.format("%s: %s.", getString(R.string.empty_fields), etModel.getText()), Toast.LENGTH_LONG);
+            etModel.requestFocus();
+        }
+    }
+
+    private void validateColor() {
+        if (etColor.getText() == null || etColor.getText().length() == 0) {
+            showToast(String.format("%s: %s.", getString(R.string.empty_fields), etColor.getText()), Toast.LENGTH_LONG);
+            etColor.requestFocus();
+        }
+    }
+
+    private void validateManufactoringYear() {
+        if (etManufactoringYear.getText() == null || etManufactoringYear.getText().length() == 0) {
+            showToast(String.format("%s: %s.", getString(R.string.empty_fields), etManufactoringYear.getText()), Toast.LENGTH_LONG);
+            etManufactoringYear.requestFocus();
         }
     }
 
