833ea27fa70b66cabee9486d14d365ebe4033bdc
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-09-04 15:27:23 -0300

    MOV - Adding pricePerLiter and kilometersPerLiter

--- modulo-04/CETEJ36-Desenvolvimento_Java_Para_Dispositivos_Moveis/AutomobileSupplyControl/app/src/main/java/com/ogawalucas/automobilesupplycontrol/supply/model/Supply.java
+++ modulo-04/CETEJ36-Desenvolvimento_Java_Para_Dispositivos_Moveis/AutomobileSupplyControl/app/src/main/java/com/ogawalucas/automobilesupplycontrol/supply/model/Supply.java
@@ -7,6 +7,9 @@ import androidx.room.PrimaryKey;
 
 import com.ogawalucas.automobilesupplycontrol.automobile.model.Automobile;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Entity(tableName = "supply",
     foreignKeys = @ForeignKey(
         entity = Automobile.class,
@@ -34,6 +37,10 @@ public class Supply {
 
     private String amountPaid;
 
+    private String pricePerLiter;
+
+    private String kilometersPerLiter;
+
     public Supply(long automobileId, String fuelStation, String date, String typeOfFuel, String kilometers, String liters, String amountPaid) {
         this.automobileId = automobileId;
         this.fuelStation = fuelStation;
@@ -42,6 +49,16 @@ public class Supply {
         this.kilometers = kilometers;
         this.liters = liters;
         this.amountPaid = amountPaid;
+        this.pricePerLiter = calculatePricePerLiter();
+        this.kilometersPerLiter = calculateKilometersPerLiter();
+    }
+
+    private String calculatePricePerLiter() {
+        return new BigDecimal(this.amountPaid).divide(new BigDecimal(this.liters)).setScale(2, RoundingMode.HALF_DOWN).toString();
+    }
+
+    private String calculateKilometersPerLiter() {
+        return new BigDecimal(this.kilometers).divide(new BigDecimal(this.liters)).setScale(2, RoundingMode.HALF_DOWN).toString();
     }
 
     public long getId() {
@@ -106,6 +123,22 @@ public class Supply {
 
     public void setAmountPaid(String amountPaid) {
         this.amountPaid = amountPaid;
+    }
+
+    public String getPricePerLiter() {
+        return pricePerLiter;
+    }
+
+    public void setPricePerLiter(String pricePerLiter) {
+        this.pricePerLiter = pricePerLiter;
+    }
+
+    public String getKilometersPerLiter() {
+        return kilometersPerLiter;
+    }
+
+    public void setKilometersPerLiter(String kilometersPerLiter) {
+        this.kilometersPerLiter = kilometersPerLiter;
     }
 }
 
